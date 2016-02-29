652c4abb08c2ac15e466b91ee039f28a724cb38d
wallellen <wallellen@hotmail.com>
2016-02-29 18:03:46 +0800

    add license and correct the enter button from crlf 2 lf

--- wechat-base/src/main/java/com/wallellen/wechat/common/util/RandomUtils.java
+++ wechat-base/src/main/java/com/wallellen/wechat/common/util/RandomUtils.java
@@ -1,3 +1,36 @@
+/*
+ * This file Copyright (c) 2016. Walle.
+ * (http://www.wallellen.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the
+ * Walle Agreement (WA) and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or WA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Walle Agreement (WA), this file
+ * and the accompanying materials are made available under the
+ * terms of the WA which accompanies this distribution, and
+ * is available at http://www.wallellen.com/agreement.html
+ *
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+ */
+
 package com.wallellen.wechat.common.util;
 
 public class RandomUtils {
--- wechat-base/src/main/java/com/wallellen/wechat/common/util/res/StringManager.java
+++ wechat-base/src/main/java/com/wallellen/wechat/common/util/res/StringManager.java
@@ -1,18 +1,34 @@
 /*
- *  Licensed to the Apache Software Foundation (ASF) under one or more
- *  contributor license agreements.  See the NOTICE file distributed with
- *  this work for additional information regarding copyright ownership.
- *  The ASF licenses this file to You under the Apache License, Version 2.0
- *  (the "License"); you may not use this file except in compliance with
- *  the License.  You may obtain a copy of the License at
+ * This file Copyright (c) 2016. Walle.
+ * (http://www.wallellen.com). All rights reserved.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * This file is dual-licensed under both the
+ * Walle Agreement (WA) and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or WA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Walle Agreement (WA), this file
+ * and the accompanying materials are made available under the
+ * terms of the WA which accompanies this distribution, and
+ * is available at http://www.wallellen.com/agreement.html
+ *
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
  */
 
 package com.wallellen.wechat.common.util.res;
